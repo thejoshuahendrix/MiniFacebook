7f00b63ac20d1b424e1cd07e73f021b0a1b44d14
thejoshuahendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-12-22 06:08:49 -0500

    multilogging

--- server/src/services/Logger.ts
+++ server/src/services/Logger.ts
@@ -1,46 +1,22 @@
+import { join } from 'path';
 import { createLogger } from '@lvksh/logger';
+import { FileLogger, FileLoggerConfig } from '@lvksh/logger/lib/FileLog';
 import chalk from 'chalk';
 
-const log = createLogger(
-    {
-        http: chalk.gray`[HTTP]`,
-        data: {
-            label: "\{" + chalk.yellow`\{` + chalk.yellowBright`DB` + chalk.yellow`\}` + "\}",
-            newLine: chalk.yellowBright.bold`┣━`,
-            paddingChar: chalk.yellowBright`━`,
-            newLineEnd: chalk.yellow.bold`┗━`,
-        },
-        error: {
-            label: "[" + chalk.bgRed.white`ERROR` + "]",
-            paddingChar: chalk.red.bold`━`,
-            divider: chalk.red.bold`|`,
-            newLine: chalk.red.bold`┣━`,
-            newLineEnd: chalk.red.bold`┗━`,
-        },
-        debug: {
-            label: chalk.magenta.bold`(DEBUG)`,
-            paddingChar: chalk.magenta.bold`~`,
-            divider: chalk.magenta.bold`?`,
-            newLine: chalk.magenta.bold`|`,
-            newLineEnd: chalk.magenta.bold`\\`,
-        },
-        info: {
-            label: "[" + chalk.blue`INFO` + "]",
-            paddingChar: chalk.blue`:`,
-            divider: chalk.blue.bold`>`,
-            newLine: chalk.blue.bold`┣━`,
-            newLineEnd: chalk.blue.bold`┗━`,
-        },
-        test: {
-            label: chalk.white`<TEST>:` + new Date().toLocaleTimeString().replace(/(AM|PM)/, "",).trim(),
-            paddingChar: "-",
-            divider: chalk.white.bold`|`,
-            newLine: chalk.white.bold`┣━`,
-            newLineEnd: chalk.white.bold`┗━`,
-        },
-    },
-    { padding: "APPEND", divider: chalk.greenBright`>`, paddingChar: ":" },
-);
+const fileConfig: FileLoggerConfig = {
+    mode: 'NEW_FILE',
+    path: join(__dirname, '../logs'),
+    namePattern: 'test.txt'
+}
+const methodConfig = {
+    OK: chalk.blue('OK'),
+    INFO: 'INFO'
+}
 
+const log = createLogger(
+    methodConfig,
+    { divider: ' | ' },
+    [FileLogger(fileConfig), console.log]
+);
 
 export default log;
